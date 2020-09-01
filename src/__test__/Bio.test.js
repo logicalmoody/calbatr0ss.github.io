fd482d47362e362545ef3ac4fc9745f898a6df61
Calvin Moody <calvinmcm@gmail.com>
2020-08-31 22:30:31 -0400

    test

--- src/__test__/Bio.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { render } from "@testing-library/react"
-import Bio from "components/Bio"
-
-it.todo("should have tests")
-// it("should render my name in the bio", () => {
-// 	const { getByText } = render(<Bio />)
-// 	expect(getByText(/Calvin/)).toBeInTheDocument()
-// })
--- src/constants/constants.js
+++
@@ -1,5 +0,0 @@
-export const gitHubUrl = "https://github.com/calbatr0ss"
-export const instagramUrl = "https://www.instagram.com/calbatr0ss"
-export const twitterUrl = "https://twitter.com/calbatr0ss"
-export const resumeUrl =
-	"https://drive.google.com/file/d/1lroD0OpJ-5mUiOaCcS8VDTBzkc_hOsUM/view?usp=sharing"
