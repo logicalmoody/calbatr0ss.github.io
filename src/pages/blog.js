35c79d4d439f32cc83664cdd8928c9c638dbfcc7
Calvin Moody <calvinmcm@gmail.com>
2020-10-12 10:21:27 -0400

    add filter tags

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,55 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { graphql } from "gatsby"
 import Typography from "@material-ui/core/Typography"
+import Card from "@material-ui/core/Card"
+import CardContent from "@material-ui/core/CardContent"
+import Chip from "@material-ui/core/Chip"
 import Link from "@material-ui/core/Link"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 export default function Blog({ data }) {
-	const posts = data.allMarkdownRemark.edges
+	const allPosts = data.allMarkdownRemark.edges
+	const [allTags, setAllTags] = useState(new Set())
+	const [posts, setPosts] = useState(allPosts)
+	const [filters, setFilters] = useState(new Set())
+
+	const handleClick = (filterName) => () => {
+		const newSet = new Set(filters)
+		if (newSet.has(filterName)) {
+			newSet.delete(filterName)
+		} else {
+			newSet.add(filterName)
+		}
+		setFilters(newSet)
+	}
+
+	useEffect(() => {
+		const newSet = new Set()
+		allPosts.forEach(
+			({
+				node: {
+					frontmatter: { tags },
+				},
+			}) => {
+				tags.split(",").forEach((tag) => {
+					newSet.add(tag)
+				})
+			}
+		)
+		setAllTags(newSet)
+	}, [allPosts])
+
+	useEffect(() => {
+		if (filters.size > 0) {
+			const filteredPosts = allPosts.filter(({ node: { frontmatter: { tags } } }) =>
+				tags.split(",").some((tag) => filters.has(tag))
+			)
+			setPosts(filteredPosts)
+		} else {
+			setPosts(allPosts)
+		}
+	}, [filters, allPosts])
 
 	return (
 		<Layout>
@@ -17,13 +60,47 @@ export default function Blog({ data }) {
 				pathname="/blog"
 			/>
 			<Typography variant="h3">The CalZone</Typography>
-			<Typography variant="subtitle1" gutterBottom>
+			<Typography variant="subtitle1">
 				I write about stuff I learn, projects I work on, or thoughts I feel are worth sharing.
 			</Typography>
+
+			{[...allTags].map((tag) => (
+				<Chip
+					key={`filter-chip-${tag}`}
+					label={tag}
+					clickable
+					color={filters.has(tag) ? "primary" : undefined}
+					onClick={handleClick(tag)}
+					style={{ margin: "16px 8px" }}
+				/>
+			))}
+
+			{/* <div style={{ display: "flex" }}> */}
 			{posts.map(({ node }) => {
 				const slug = node.frontmatter.slug
 				const title = node.frontmatter.title || slug
 				return (
+					// <Card style={{ width: 300, margin: 8 }}>
+					// 	<article>
+					// 		<CardContent>
+					// 			<header>
+					// 				<Typography variant="h5">
+					// 					<Link href={slug}>{title}</Link>
+					// 				</Typography>
+					// 				<Typography variant="subtitle2" gutterBottom>
+					// 					{node.frontmatter.date}
+					// 				</Typography>
+					// 			</header>
+					// 			<section>
+					// 				<Typography
+					// 					dangerouslySetInnerHTML={{
+					// 						__html: node.frontmatter.description || node.excerpt,
+					// 					}}
+					// 				/>
+					// 			</section>
+					// 		</CardContent>
+					// 	</article>
+					// </Card>
 					<article key={slug} style={{ paddingBottom: 24 }}>
 						<header>
 							<Typography variant="h4">
@@ -41,6 +118,7 @@ export default function Blog({ data }) {
 					</article>
 				)
 			})}
+			{/* </div> */}
 		</Layout>
 	)
 }
@@ -61,6 +139,7 @@ export const pageQuery = graphql`
 						slug
 						title
 						description
+						tags
 					}
 				}
 			}
